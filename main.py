f35a1ed2f73ff059ba5a70f667fc3bab5bead6e8
anajferraz11 <ana.j.melo11@aluno.senai.br>
2026-02-06 14:43:52 -0300

    em andamento

---
+++ main.py
@@ -0,0 +1,17 @@
+import flet as ft
+
+def main(page: ft.Page):
+    ft.Container(
+        container=ft.Column(
+            controns=[
+                ft.Row(),
+                ft.Row(),
+                ft.Row(),
+                ft.Row(),
+                ft.Row()
+                
+            ]
+        )
+    )
+    
+ft.run(main)
